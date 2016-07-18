e2b4af1919f71b2ddf433e1767eaef5296a27356
David Lizárraga <dlizarra5@gmail.com>
2016-07-18 17:09:00 +0200

    Added search box component

---
+++ src/app/search-box/search-box.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+@Component({
+  moduleId: module.id,
+  selector: 'dl-search-box',
+  templateUrl: 'search-box.component.html',
+  styleUrls: ['search-box.component.css']
+})
+export class SearchBoxComponent{
+
+  constructor() {}
+
+}
