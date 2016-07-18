8ab33dd3ffbfc242f2ed800958d6b76db63c5fb4
David Lizárraga <dlizarra5@gmail.com>
2016-07-18 15:12:33 +0200

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { HeaderComponent }from './header';
 
 @Component({
   moduleId: module.id,
   selector: 'dl-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+  styleUrls: ['app.component.css'],
+  directives: [HeaderComponent]
+
 })
 export class AppComponent {
   title = 'app works!';
--- src/system-config.ts
+++ src/system-config.ts
@@ -34,6 +34,7 @@ const barrels: string[] = [
   // App specific barrels.
   'app',
   'app/shared',
+  'app/header',
   /** @cli-barrel */
 ];
 
