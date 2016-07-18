85d4a046960b01ac0b9e0a224696355cfa79a404
David Lizárraga <dlizarra5@gmail.com>
2016-07-18 20:32:39 +0200

    Disabled old forms module and enabled new one. We only need to do this until rc 5.

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
+import { disableDeprecatedForms, provideForms } from '@angular/forms';
 import { AppComponent, environment } from './app/';
 
 if (environment.production) {
   enableProdMode();
 }
 
-bootstrap(AppComponent);
+// TODO remove disableDeprecatedForms and provideForms when on rc 5.
+bootstrap(AppComponent, [
+  disableDeprecatedForms(),
+  provideForms()
+]);
 
