c6b6ab9f1ba9e29bfb1bdbbada648c60115040fa
David Lizárraga <dlizarra5@gmail.com>
2016-07-20 21:51:42 +0200

    new gh-pages version

---
+++ system-config.js
@@ -0,0 +1,60 @@
+// SystemJS configuration file, see links for more information
+// https://github.com/systemjs/systemjs
+// https://github.com/systemjs/systemjs/blob/master/docs/config-api.md
+/***********************************************************************************************
+ * User Configuration.
+ **********************************************************************************************/
+/** Map relative paths to URLs. */
+var map = {};
+/** User packages configuration. */
+var packages = {
+    'moment': {
+        format: 'cjs'
+    }
+};
+////////////////////////////////////////////////////////////////////////////////////////////////
+/***********************************************************************************************
+ * Everything underneath this line is managed by the CLI.
+ **********************************************************************************************/
+var barrels = [
+    // Angular specific barrels.
+    '@angular/core',
+    '@angular/common',
+    '@angular/compiler',
+    '@angular/forms',
+    '@angular/http',
+    '@angular/router',
+    '@angular/platform-browser',
+    '@angular/platform-browser-dynamic',
+    // Thirdparty barrels.
+    'rxjs',
+    // App specific barrels.
+    'app',
+    'app/shared',
+    'app/shared/nav',
+    'app/shared/nav/header',
+    'app/shared/nav/search-box',
+    'app/search-box',
+    'app/videos',
+    'app/videos/video-detail',
+    'app/videos/video-list',
+    'app/videos/video-list-item',
+    'app/videos/shared'
+];
+var cliSystemConfigPackages = {};
+barrels.forEach(function (barrelName) {
+    cliSystemConfigPackages[barrelName] = { main: 'index' };
+});
+// Apply the CLI SystemJS configuration.
+System.config({
+    map: {
+        '@angular': 'vendor/@angular',
+        'rxjs': 'vendor/rxjs',
+        'main': 'main.js',
+        'moment': 'vendor/moment/moment.js'
+    },
+    packages: cliSystemConfigPackages
+});
+// Apply the user's configuration.
+System.config({ map: map, packages: packages });
+//# sourceMappingURL=system-config.js.map
